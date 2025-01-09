d857591c31518a4c5b1131d0ad6d771eb24e214d
Anthony Maxwell <anthony.maxwell@automox.com>
2025-01-09 12:19:22 -0500

    Session: 1/9/25

---
+++ interfaces/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+type Transform interface {
+	Apply(string) string
+}
+
+func Log(msg string, transforms ...Transform) {
+	for _, transform := range transforms {
+		msg = transform.Apply(msg)
+	}
+
+	_, _ = os.Stdout.Write([]byte(msg))
+	fmt.Fprintln(os.Stdout, msg)
+}
+
+var _ Transform = LogTimestamp{}
+
+type LogTimestamp struct {
+	Format string
+}
+
+func (l LogTimestamp) Apply(v string) string {
+	var (
+		now     = time.Now()
+		dateStr = now.Format(l.Format)
+	)
+
+	return fmt.Sprintf("%s %s", dateStr, v) // dateStr + " " + v
+}
+
+func main() {
+	var lts = LogTimestamp{
+		Format: "2006-01-02 03:04:05",
+	}
+
+	Log("Hello, world!", lts)
+	Log("Hello, world!", lts)
+	Log("Hello, world!", lts)
+	Log("Hello, world!", lts)
+	Log("Hello, world!", lts)
+}
